39a5b4462e403b44aabc8122cda679faac5b4465
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2025-01-04 16:01:57 +0900

    feat: 네비게이션, 화면생성 -#1

---
+++ common/Navigation.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import CalendarScreen from '../screens/CalendarScreen';
+import LibraryScreen from '../screens/LibraryScreen';
+import MypageScreen from '../screens/MypageScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="home"
+        screenOptions={{
+          tabBarActiveTintColor: '#000',
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          name="home"
+          component={HomeScreen}
+          options={{
+            title: 'HOME',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name={'home'} color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Calendar"
+          component={CalendarScreen}
+          options={{
+            title: 'CALENDAR',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name={'calendar-month'} color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Library"
+          component={LibraryScreen}
+          options={{
+            title: 'LIBRARY',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name={'grade'} color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Mypage"
+          component={MypageScreen}
+          options={{
+            title: 'MYPAGE',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name={'person'} color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
---
+++ screens/MypageScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MypageScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>MypageScreen</Text>
+    </View>
+  );
+};
+
+export default MypageScreen;
